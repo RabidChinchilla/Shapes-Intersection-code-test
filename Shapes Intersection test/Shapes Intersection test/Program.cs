779d096429b89f4abe190a0c3430fd322ee740d0
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2021-02-16 11:45:20 +0000

    started work

---
+++ Shapes Intersection test/Shapes Intersection test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shapes_Intersection_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public Dictionary<int, List<int>> FindIntersections(List<Shape> shapes)
+        {
+
+        }
+    }
+}
