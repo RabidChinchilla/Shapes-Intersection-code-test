59e690f58ef76a766ea26b48dee970f584da9088
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2021-02-16 14:47:57 +0000

    list created now to work on dictionary

--- Shapes Intersection test/Shapes Intersection test/Program.cs
+++ Shapes Intersection test/Shapes Intersection test/Program.cs
@@ -11,26 +11,29 @@ namespace Shapes_Intersection_test
             Shape rectangle1 = new Shape(2, 55, 70, 170, 90);
             Shape rectangle2 = new Shape(3, 90, 120, 20, 90);
             Shape circle = new Shape(1, 20, 20, 90, 90);
+
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(rectangle1);
+            shapes.Add(rectangle2);
+            shapes.Add(circle);
+
             Console.WriteLine("Shapes made");
-            Console.WriteLine(rectangle1.ID);
-            Console.WriteLine(rectangle2.ID);
-            Console.WriteLine(circle.ID);
+            shapes.ForEach(i => Console.WriteLine("{0}\t", i.ID));
+            shapes.ForEach(i => Console.WriteLine("{0}\t", i.dimensions));
+
         }
 
-        //public Dictionary<int, List<int>> FindIntersections(List<Shape> shapes)
-        //{
-        //    return shapes;
-        //}
+        public Dictionary<int, List<int>> FindIntersections(List<Shape> shapes)
+        {
+            return shapes;
+        }
     }
 
     public class Shape
     {
         public int ID;
         public Rectangle dimensions;
-        //public int X;
-        //public int Y;
-        //public int Height;
-        //public int Width;
+
 
         public Shape(int id, int x, int y, int height, int width)
         {
@@ -39,10 +42,6 @@ namespace Shapes_Intersection_test
             dimensions.Y = y;
             dimensions.Height = height;
             dimensions.Width = width;
-            //X = x;
-            //Y = y;
-            //Height = height;
-            //Width = width;
         }
     }
 }
